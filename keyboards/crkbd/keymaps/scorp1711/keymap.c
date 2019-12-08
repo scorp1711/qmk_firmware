b6561c6b5ac7b2969e14623390f20969077d3fe3
Christian Fressl <christian.fressl@gmail.com>
2019-12-08 22:07:19 +0100

    add lock key

--- keyboards/crkbd/keymaps/scorp1711/keymap.c
+++ keyboards/crkbd/keymaps/scorp1711/keymap.c
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT,  KC_INS,  KC_DEL,\
+      KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, LGUI(KC_L), XXXXXXX,                   KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT,  KC_INS,  KC_DEL,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9, KC_HOME,  KC_END,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
