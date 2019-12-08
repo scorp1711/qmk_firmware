6dbce2f7d2d87adcff597a0b9bf2ebefbc85a340
Christian Fressl <christian.fressl@gmail.com>
2019-12-08 23:42:15 +0100

    latest keymap changes, added page up/down

--- keyboards/crkbd/keymaps/scorp1711/keymap.c
+++ keyboards/crkbd/keymaps/scorp1711/keymap.c
@@ -46,11 +46,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_LOWER] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,\
+       KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_DEL,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, LGUI(KC_L), XXXXXXX,                   KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT,  KC_INS,  KC_DEL,\
+      KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, LGUI(KC_L), XXXXXXX,                   KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT,  KC_INS, KC_PGUP,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9, KC_HOME,  KC_END,\
+      KC_LSFT,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, KC_PGDN,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                     LALT_T(KC_LGUI), LOWER,  KC_SPC,     KC_ENT,   RAISE, KC_RALT \
                                       //`--------------------------'  `--------------------------'
